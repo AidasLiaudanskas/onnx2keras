eed9dc3970eb1207c2ed4169e8634b693ef97828
AidasLiaudanskas <aidas.liaud@gmail.com>
2020-06-30 14:09:33 +0300

    Fixed upsample scales being passed in as a second input

--- onnx2keras/upsampling_layers.py
+++ onnx2keras/upsampling_layers.py
@@ -17,17 +17,27 @@ def convert_upsample(node, params, layers, lambda_func, node_name, keras_name):
     logger = logging.getLogger('onnx2keras:upsample')
     logger.warning('!!! EXPERIMENTAL SUPPORT (upsample) !!!')
 
-    if len(node.input) != 1:
-        raise AttributeError('Unsupported number of inputs')
+    if len(node.input) == 1:
+        scale = np.uint8(params['scales'][-2:])
+    elif len(node.input) == 2:
+        scale = np.uint8(layers[node.input[1]][-2:])
+        logger.warning(
+            "Found two inputs, assuming second input is the upsampling scale!")
+    else:
+        raise AttributeError(
+            'Unsupported number of inputs. Expected 1 or 2, got {}. {}'.format(
+                len(node.input), node.input))
+    interpolation_type = params['mode'].decode('utf-8')
+    interpolation_type = 'bilinear' if 'linear' in interpolation_type else interpolation_type
 
-    if params['mode'].decode('utf-8') != 'nearest':
-        logger.error('Cannot convert non-nearest upsampling.')
-        raise AssertionError('Cannot convert non-nearest upsampling')
+    if interpolation_type not in ['nearest', 'bilinear']:
+        logger.warning(
+            'Cannot convert non-nearest upsampling. Got {}. Might not work properly'
+            .format(interpolation_type))
+    # TODO: might need to force nearest upsampling here? Works in my case..
 
-    scale = np.uint8(params['scales'][-2:])
-
-    upsampling = keras.layers.UpSampling2D(
-        size=scale, name=keras_name
-    )
+    upsampling = keras.layers.UpSampling2D(size=scale,
+                                           name=keras_name,
+                                           interpolation=interpolation_type)
 
     layers[node_name] = upsampling(layers[node.input[0]])

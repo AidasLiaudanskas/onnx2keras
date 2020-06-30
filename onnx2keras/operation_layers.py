a128301505cd90008f3e171b8f79c491da90d35a
AidasLiaudanskas <aidas.liaud@gmail.com>
2020-06-30 14:08:48 +0300

    Fixed axis in L2 reduce

--- onnx2keras/operation_layers.py
+++ onnx2keras/operation_layers.py
@@ -26,15 +26,18 @@ def convert_clip(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for clip layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     if params['min'] == 0:
         logger.debug("Using ReLU({0}) instead of clip".format(params['max']))
         layer = keras.layers.ReLU(max_value=params['max'], name=keras_name)
     else:
+
         def target_layer(x, vmin=params['min'], vmax=params['max']):
             import tensorflow as tf
             return tf.clip_by_value(x, vmin, vmax)
+
         layer = keras.layers.Lambda(target_layer, name=keras_name)
         lambda_func[keras_name] = target_layer
 
@@ -55,7 +58,8 @@ def convert_log(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for log layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     def target_layer(x):
         import tensorflow.keras.backend as K
@@ -79,7 +83,8 @@ def convert_exp(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for log layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     def target_layer(x):
         import tensorflow.keras.backend as K
@@ -90,7 +95,8 @@ def convert_exp(node, params, layers, lambda_func, node_name, keras_name):
     lambda_func[keras_name] = target_layer
 
 
-def convert_reduce_sum(node, params, layers, lambda_func, node_name, keras_name):
+def convert_reduce_sum(node, params, layers, lambda_func, node_name,
+                       keras_name):
     """
     Convert reduce sum.
     :param node: current operation node
@@ -104,7 +110,8 @@ def convert_reduce_sum(node, params, layers, lambda_func, node_name, keras_name)
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for reduce sum layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     axis = params['axes']
 
@@ -118,7 +125,8 @@ def convert_reduce_sum(node, params, layers, lambda_func, node_name, keras_name)
     lambda_func[keras_name] = target_layer
 
 
-def convert_reduce_mean(node, params, layers, lambda_func, node_name, keras_name):
+def convert_reduce_mean(node, params, layers, lambda_func, node_name,
+                        keras_name):
     """
     Convert reduce mean.
     :param node: current operation node
@@ -132,7 +140,8 @@ def convert_reduce_mean(node, params, layers, lambda_func, node_name, keras_name
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for reduce mean layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     def target_layer(x, axis=params['axes'], keepdims=params['keepdims']):
         import tensorflow.keras.backend as K
@@ -158,7 +167,8 @@ def convert_pow(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 2:
         assert AttributeError('More than 2 inputs for pow layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
     power = ensure_numpy_type(layers[node.input[1]])
 
     def target_layer(x, a=power):
@@ -184,7 +194,8 @@ def convert_sqrt(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for sqrt layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     def target_layer(x):
         import tensorflow.keras.backend as K
@@ -209,19 +220,20 @@ def convert_split(node, params, layers, lambda_func, node_name, keras_names):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for split layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_names[0])
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_names[0])
     splits = params["split"]
     axis = params.get("axis", 0)
     if not isinstance(splits, Iterable):
         # This might not work if `split` is a tensor.
         chunk_size = K.int_size(input_0)[axis] // splits
-        splits = (chunk_size,) * splits
+        splits = (chunk_size, ) * splits
 
     cur = 0
     for i, split in enumerate(splits):
         node_name = params['_outputs'][i]
 
-        def target_layer(x, axis=axis, start_i=cur, end_i=cur+split):
+        def target_layer(x, axis=axis, start_i=cur, end_i=cur + split):
             slices = [slice(None, None)] * len(K.int_shape(x))
             slices[axis] = slice(start_i, end_i)
             return x[tuple(slices)]
@@ -265,7 +277,8 @@ def convert_cast(node, params, layers, lambda_func, node_name, keras_name):
 
         layers[node_name] = cast_map[params['to']](node.input[0])
     else:
-        input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+        input_0 = ensure_tf_type(layers[node.input[0]],
+                                 name="%s_const" % keras_name)
 
         def target_layer(x, dtype=params['to']):
             import tensorflow as tf
@@ -301,7 +314,8 @@ def convert_floor(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for floor layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
 
     def target_layer(x):
         # Floor is absent in keras.backend
@@ -344,7 +358,8 @@ def convert_argmax(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for argmax layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
     axis = params.get("axis", -1)
 
     def target_layer(x, axis=axis):
@@ -356,7 +371,8 @@ def convert_argmax(node, params, layers, lambda_func, node_name, keras_name):
     lambda_func[keras_name] = target_layer
 
 
-def convert_reduce_l2(node, params, layers, lambda_func, node_name, keras_name):
+def convert_reduce_l2(node, params, layers, lambda_func, node_name,
+                      keras_name):
     """
     Convert ReduceL2 layer
     :param node: current operation node
@@ -370,8 +386,10 @@ def convert_reduce_l2(node, params, layers, lambda_func, node_name, keras_name):
     if len(node.input) != 1:
         assert AttributeError('More than 1 input for reduce_l2 layer.')
 
-    input_0 = ensure_tf_type(layers[node.input[0]], name="%s_const" % keras_name)
-    axis = params.get("axes", [-1])
+    input_0 = ensure_tf_type(layers[node.input[0]],
+                             name="%s_const" % keras_name)
+    axis = params.get("axes", [-1])[0]
+    print('axis: ', axis)
 
     def target_layer(x, axis=axis):
         import tensorflow as tf
